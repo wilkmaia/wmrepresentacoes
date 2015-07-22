d13def87f87679966ed6f6d6b962656bb8e483f7
Wilk Maia <wilkmaia@gmail.com>
2015-07-22 11:55:08 -0300

    Novos Testes (indexedDB)

--- js/script.js
+++ js/script.js
@@ -1,3 +1,34 @@
+// Criação do Banco de Dados
+var db = new Dexie("WMRepresentacoes");
+db.version(1).stores({
+	clientes: "++id, razaoSocial, cnpj",
+	representadas: "++id, razaoSocial, cnpj"
+});
+db.open();
+
+function showAllLocalStorage() {
+	db.clientes.each(function(c){
+		console.log("Cliente: " + c.razaoSocial + " - CNPJ: " + c.cnpj );
+	});
+}
+
 $(document).ready(function(){
-	$("#content").hide(5000);
+	
+	$(".cnpj_field").mask("99.999.999/9999-99");
+	
+	
+	$("#_form").submit(function( e ){
+		e.preventDefault();
+		
+		db.clientes.add({
+			razaoSocial: $("#_razaoSocial").val(),
+			cnpj: $("#_cnpj").val()
+		});
+		
+		$("#_form")[0].reset();
+	});
+	
+	$("#_clear").click(function(){
+		$("#_form")[0].reset();
+	});
 })
